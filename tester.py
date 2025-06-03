f96af069485d31ea10c953f917a49b1b55a82ec2
FirstName LastName <username@amazon.com>
2025-06-03 14:58:04 +0530

    rjrjrnjn

--- tester.py
+++ tester.py
@@ -1,15 +1,15 @@
-import csv
 import logging
-import random
-import typing
 import numpy as np
+import csv
+import typing
+import random
 
-from django.conf import settings
 from django.core.cache import cache
 from django.http import HttpResponse
+from django.conf import settings
 
-from project.models.user import Profile
 from project.services import analytics
+from project.models.user import Profile
 from project.utils.helpers import format_response
 
 def main():
