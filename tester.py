cfe45d9e2cc64d6f850041d5a019554945097987
FirstName LastName <username@amazon.com>
2025-06-03 15:03:22 +0530

    f

--- tester.py
+++ tester.py
@@ -4,13 +4,13 @@ import csv
 import typing
 import random
 
-from django.core.cache import cache
-from django.http import HttpResponse
-from django.conf import settings
-
 from project.services import analytics
 from project.models.user import Profile
 from project.utils.helpers import format_response
+
+from django.core.cache import cache
+from django.http import HttpResponse
+from django.conf import settings
 
 def main():
     pass
